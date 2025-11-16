ba5b41e61f00677846a580883c21f8d992854313
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2025-11-16 13:43:54 +0200

    Added the generate random word method

--- Couriers.Common.Xml.Tests/TestHelpers.cs
+++ Couriers.Common.Xml.Tests/TestHelpers.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Couriers.Common.Xml.Tests
 {
@@ -12,14 +14,41 @@ namespace Couriers.Common.Xml.Tests
         /// <summary>
         /// The digits and letters contained in the English alphabet
         /// </summary>
-        private static readonly char[] _lettersAndDigits =
+        private static readonly char[] _lettersAndDigits;
+
+        /// <summary>
+        /// The letters contained in the English alphabet
+        /// </summary>
+        private static readonly char[] _letters =
         [
             'a', 'b', 'c', 'd', 'e', 'f',
             'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-            'w', 'x', 'y', 'z', '0', '1', '2', '3',
+            'w', 'x', 'y', 'z'
+        ];
+
+        /// <summary>
+        /// The digits
+        /// </summary>
+        private static readonly char[] _digits =
+        [
+            '0', '1', '2', '3',
             '4', '5', '6', '7', '8', '9'
         ];
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TestHelpers"/>
+        /// </summary>
+#pragma warning disable S3963 // "static" fields should be initialized inline
+        static TestHelpers()
+#pragma warning restore S3963 // "static" fields should be initialized inline
+        {
+            _lettersAndDigits = [.. _letters, .. _digits];
+        }
 
         #endregion
 
@@ -41,6 +70,27 @@ namespace Couriers.Common.Xml.Tests
                 var randomCharacterIndex = Random.Shared.Next(_lettersAndDigits.Length - 1);
 
                 span[i] = _lettersAndDigits[randomCharacterIndex];
+            }
+
+            return new string(span);
+        }
+
+        /// <summary>
+        /// Generates a random <see cref="string"/> of length <paramref name="length"/>
+        /// </summary>
+        /// <param name="length">The length of the returned string</param>
+        /// <returns></returns>
+        public static string RenerateRandomWord(int length)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(length);
+
+            Span<char> span = stackalloc char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                var randomCharacterIndex = Random.Shared.Next(_letters.Length - 1);
+
+                span[i] = _letters[randomCharacterIndex];
             }
 
             return new string(span);
